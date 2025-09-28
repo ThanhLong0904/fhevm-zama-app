8d29e35a095d512f91b08114642d7e7f85a8b1ee
Song Le <lesong0162@gmail.com>
2025-09-28 17:46:27 +0700

    - Update

--- Frontend/abi/FHECounterAddresses.ts
+++ Frontend/abi/FHECounterAddresses.ts
@@ -4,6 +4,6 @@
   Command: 'npm run genabi'
 */
 export const FHECounterAddresses = { 
-  "11155111": { address: "0xf2BDd3B47Bd29d6098D4721c1c4776965C51B877", chainId: 11155111, chainName: "sepolia" },
-  "31337": { address: "0x5FbDB2315678afecb367f032d93F642f64180aa3", chainId: 31337, chainName: "hardhat" },
+  "11155111": { address: "0xAC899AD001A04c18863F33294B375Fa89EC1d3B3", chainId: 11155111, chainName: "sepolia" },
+  "31337": { address: "0xAC899AD001A04c18863F33294B375Fa89EC1d3B3", chainId: 31337, chainName: "hardhat" },
 };
--- Frontend/abi/VotingRoomAddresses.ts
+++ Frontend/abi/VotingRoomAddresses.ts
@@ -4,6 +4,6 @@
   Command: 'npm run genabi'
 */
 export const VotingRoomAddresses = { 
-  "11155111": { address: "0x9754476eC3f6Ae1302E341F3951D9F5a7404B334", chainId: 11155111, chainName: "sepolia" },
-  "31337": { address: "0xfC4C6F6ABE4998b7ec618AB716Cf631cC6F35B06", chainId: 31337, chainName: "hardhat" },
+  "11155111": { address: "0x28eD335833DD3Ecc88a03d98E99a370c44f95bCD", chainId: 11155111, chainName: "sepolia" },
+  "31337": { address: "0x28eD335833DD3Ecc88a03d98E99a370c44f95bCD", chainId: 31337, chainName: "hardhat" },
 };
